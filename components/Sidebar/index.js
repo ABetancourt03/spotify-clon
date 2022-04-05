fc5c763f4e6b1d66c75535141ff6b5030115b9fe
ABetancourt03 <neutralzgamer@gmail.com>
2022-04-04 20:16:33 -0400

    First Commit

---
+++ components/Sidebar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <aside>
+      <h2>Spotify</h2>
+    </aside>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+export default function MyApp ({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+      <style jsx global>{`
+        html {
+          box-sizing: border-box;
+          font-size: 18px;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+      `}
+      </style>
+    </>
+  )
 }
-
-export default MyApp
